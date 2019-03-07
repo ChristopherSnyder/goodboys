44e32e67bf45a4961ad1d2663cff3f9ea65fd382
Chris Snyder <christopher.snyder@aceproject.space>
2019-03-06 19:42:53 -0600

    Seeded database with randomly generated good boys

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,15 +18,31 @@ url = 'https://dog.ceo/api/breeds/list/all'
 
 #Adding to Breeds table
 
-url = 'https://dog.ceo/api/breeds/list/all'
-uri = URI(url)
-response = Net::HTTP.get(uri)
-dogs = JSON.parse(response)['message']
+# url = 'https://dog.ceo/api/breeds/list/all'
+# uri = URI(url)
+# response = Net::HTTP.get(uri)
+# dogs = JSON.parse(response)['message']
 
-newbreed = Breed.new
+# newbreed = Breed.new
 
-dogs.each do |dog|
-newbreed = Breed.new
-newbreed.name = dog[0]
-newbreed.save
+# dogs.each do |dog|
+# newbreed = Breed.new
+# newbreed.name = dog[0]
+# newbreed.save
+# end
+
+#Adding to Good Boys table
+
+for i in 0..99
+    newgoodboy = Goodboy.new
+    newname = Name.offset(rand(Name.count)).first
+    newbreed = Breed.offset(rand(Breed.count)).first
+    newsnack = Snack.offset(rand(Snack.count)).first
+
+    newgoodboy.name = newname
+    newgoodboy.breed = newbreed
+    newgoodboy.snack = newsnack
+
+    newgoodboy.save
+
 end
