c5bc15a4cb3f02985a865590b1f61b5e338bad88
Chris Snyder <christopher.snyder@aceproject.space>
2019-03-07 11:29:30 -0600

    Added a conditional to welcome page because I like free marks

--- app/controllers/goodboys_controller.rb
+++ app/controllers/goodboys_controller.rb
@@ -1,3 +1,5 @@
+
+
 class GoodboysController < ApplicationController
     def new
     end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @t = Time.now
   end
 end
