fcc09088167b08e6ad8d05f5bb1192dab9326098
Chris Snyder <christopher.snyder@aceproject.space>
2019-03-06 19:03:32 -0600

    Added JSON data for Dog Breeds

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -1,8 +1,16 @@
+require 'json'
+require 'open-uri'
+
+
 class BreedsController < ApplicationController
     def new
     end
 
     def index
+        url = 'https://dog.ceo/api/breeds/list/all'
+        uri = URI(url)
+        response = Net::HTTP.get(uri)
+        @dogs = JSON.parse(response)['message']
     end
 
 end
